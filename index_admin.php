fa807651eb63e2db45afcc6804736521a4ece099
oshantt <131771585+oshantt@users.noreply.github.com>
2023-06-11 14:11:30 +0800

    Update index_admin.php

--- index_admin.php
+++ index_admin.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['instructor_name'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="assets/icon.ico" type="image/x-icon">
-	<link rel="shortcut icon" href="assets/icon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="assets/icon.ico" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
@@ -33,12 +33,11 @@ if (!isset($_SESSION['instructor_name'])) {
       <p>Instructor</p>
     </div>
     <div class="spacer"></div>
+    <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#customQueryModal">Filter</button>
     <a href="add_event.php" class="ghost">Add Event</a>
     <a href="index_subject.php" class="ghost">View Subjects</a>
     <a href="logout.php" class="ghost">Logout</a>
   </header>
-
-
 
   <div class="container">
     <?php
@@ -50,6 +49,26 @@ if (!isset($_SESSION['instructor_name'])) {
     </div>';
     }
     ?>
+
+    <div class="modal fade" id="customQueryModal" tabindex="-1" aria-labelledby="customQueryModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="customQueryModalLabel">Filter</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form id="customQueryForm">
+              <div class="mb-3">
+                <label for="customQuery" class="form-label">Enter SQL Query</label>
+                <textarea class="form-control" id="customQuery" name="customQuery" rows="5" required>SELECT * FROM event_tbl e INNER JOIN subject_tbl s ON e.subject_id = s.subject_id;</textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">Run Filter</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <table class="table table-hover text-center">
       <thead class="table-active">
@@ -95,6 +114,8 @@ if (!isset($_SESSION['instructor_name'])) {
   <script src="js/app.js"></script>
   <script src="js/bg.js"></script>
   <script src="js/sort.js"></script>
+  <script src="js/filter.js"></script>
+
 </body>
 
 </html>
